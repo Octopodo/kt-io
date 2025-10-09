60dd1c4ea0ecfb2730c5c85364d710ba41eecefc
octopodo <orson_wells40@hotmail.com>
2025-10-09 13:12:56 +0200

    Remove IO Tests from index.test.ts to streamline test execution

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,23 +1,7 @@
-import {
-    describe,
-    it,
-    expect,
-    beforeEach,
-    afterEach,
-    runTests,
-    getSuites,
-    beforeAll,
-    afterAll,
-} from "kt-testing-suite-core";
-import { IO } from "../index";
+import { runTests } from "kt-testing-suite-core";
+
 import "./path.test";
 import "./fs.test";
 import "./utils.test";
 
-describe("IO Tests", () => {
-    it("should be able to create a new instance of IO", () => {
-        const plugin = new IO();
-        expect(plugin).toBeInstanceOf(IO);
-    });
-});
 runTests();
