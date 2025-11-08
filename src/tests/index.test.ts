c260c2c81467d418252239cb4c356477d1988bcf
octopodo <orson_wells40@hotmail.com>
2025-11-08 17:51:14 +0100

    .

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -3,5 +3,6 @@ import { runTests } from "kt-testing-suite-core";
 // import "./path.test";
 // import "./fs.test";
 // import "./createFolderTree.test";
-import "./extensionValidate.test";
+// import "./extensionValidate.test";
+import "./scanFolder.test";
 runTests();
