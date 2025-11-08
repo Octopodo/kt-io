e8b597563af5a892d96563162e0ab8af174c3120
octopodo <orson_wells40@hotmail.com>
2025-11-08 18:17:22 +0100

    Refactor exports in index.ts to include KT_Descriptor, KT_FileDescriptor, and KT_FolderDescriptor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { IO } from "./IO";
 import { KT_Path } from "./path";
 import { KT_Fs } from "./fs";
-import { KT_IoUtils } from "./utils";
+import {
+    KT_IoUtils,
+    KT_Descriptor,
+    KT_FileDescriptor,
+    KT_FolderDescriptor,
+} from "./utils";
 
-export { IO, KT_Path, KT_Fs, KT_IoUtils };
+export {
+    IO,
+    KT_Path,
+    KT_Fs,
+    KT_IoUtils,
+    KT_Descriptor,
+    KT_FileDescriptor,
+    KT_FolderDescriptor,
+};
